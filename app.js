733234af1df8c8ffdd6505ef708362ef9b16806d
Coding-Muslimah <64087365+Coding-Muslimah@users.noreply.github.com>
2020-06-09 14:19:34 +0100

    Delete app.js

--- app.js
+++
@@ -1,7 +0,0 @@
-var greetings = prompt("Would you like to check out our England Travel Destination?")
-if (greetings === "Yes"){
-    alert("Welcome to the largest country in the United Kingdom ")
-}
-else{
-    alert("Sorry, Go back to the homepage")
-}
